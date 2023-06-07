091c1f849333924c6fa32fbff2a2c957ea5bf81c
RodrigoBCGomes <rodrigosgtau@gmail.com>
2023-06-07 16:38:56 -0300

    Express funcionando

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,7 @@
-console.log("Deu certo!")
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.listen(3000,()=>{
+    console.log("Servidor rodando na porta 3000, para acessar use http://localhost:3000")
+})
